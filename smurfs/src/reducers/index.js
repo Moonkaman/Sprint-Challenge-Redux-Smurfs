76764d04258ebf2e39b6a49cbc5b25a3cc2d3d73
Moonkaman <nickbatmanglidj@gmail.com>
2019-02-01 09:34:41 -0700

    Create root reducer, applymiddleware & create SmurfsView

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,3 +1,18 @@
+const initialState = {
+  smurfs: []
+}
+
+const reducer = (state = initialState, action) => {
+  switch(action.type) {
+    default:
+      return {
+        ...state
+      }
+  }
+}
+
+export default reducer;
+
 /*
   Be sure to import in all of the action types from `../actions`
 */
---
+++ smurfs/src/views/SmurfsView.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class SmurfsView extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Smurfs</h1>
+      </div>
+    );
+  }
+}
+
+export default SmurfsView;
